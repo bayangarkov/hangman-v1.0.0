216d2c0bcfedb61a208f7b234721a9ab5d26fd55
Bayan Garkov <baiangarkov@gmail.com>
2022-08-01 20:18:00 +0300

    Adding main files
    This is not final version, still in development new features

---
+++ controller.js
@@ -0,0 +1,54 @@
+"use strict";
+
+import { words } from "./model.js";
+import { NewGame, Check, clearLocalStorage } from "./view.js";
+
+const selectedCategory = document.querySelector(".form-select");
+const btnNewGame = document.querySelector(".btn-new-game");
+const btnCheck = document.querySelector(".btn-check");
+const inputField = document.querySelector(".form-control");
+const btnClearScore = document.querySelector(".btn-clear-score");
+
+btnClearScore.addEventListener("click", function (e) {
+  e.preventDefault();
+  clearLocalStorage();
+});
+
+let randomWordArr = [];
+
+btnNewGame.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Get data from options
+  let typeCatg = selectedCategory.value;
+
+  // Use data from options
+  const createNewGame = new NewGame();
+  if (typeCatg === "Select category") return;
+
+  let rndmWord = createNewGame.createRandomWord(words.category[typeCatg]);
+  console.log(`random word: ${rndmWord}`);
+
+  randomWordArr.length = 0;
+  randomWordArr.push(rndmWord);
+
+  createNewGame.showInputField();
+  createNewGame.clearHangman();
+  createNewGame.outputWordToView(rndmWord);
+  createNewGame.clearGuesses();
+  createNewGame.updateScore();
+  createNewGame.showStarterHangman();
+});
+
+btnCheck.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (!randomWordArr.length) return;
+  let inputLetter = inputField.value.toLowerCase();
+
+  const verify = new Check();
+
+  verify.checkForEquality(randomWordArr[0], inputLetter);
+
+  inputField.value = "";
+});
